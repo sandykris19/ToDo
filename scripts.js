6cd08f23f48c5230316a069b0c110b413506dbbf
sandykris19 <35339937+sandykris19@users.noreply.github.com>
2020-12-11 10:56:19 +0530

    Added Suggestions

--- scripts.js
+++ scripts.js
@@ -1,48 +1,53 @@
-const addTodo = document.querySelector('.add')
-const todos = document.querySelector('.todos')
-const search = document.querySelector('.search input')
+const addTodo = document.querySelector(".add");
+const todos = document.querySelector(".todos");
+const search = document.querySelector(".search input");
+const helper = document.querySelector(".helper");
 
 const generateTemplate = (todo) => {
-    const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
+  const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
     <span>${todo}</span>
     <i class="far fa-trash-alt delete"></i>
-</li>`
-    todos.innerHTML += html
+</li>`;
+  todos.innerHTML += html;
+};
 
-}
+addTodo.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-addTodo.addEventListener('submit', (event) => {
-    event.preventDefault()
+  const todo = addTodo.add.value.trim();
 
-    const todo = addTodo.add.value.trim()
-    console.log(todo);
+  if (todo.length) generateTemplate(todo);
 
-    if (todo.length)
-        generateTemplate(todo)
+  addTodo.reset();
+  const slist = [
+    "Meet John",
+    "Make pasta",
+    "Order pizza",
+    "Clean Bedroom",
+    "Complete homework",
+  ];
+  let random = slist[Math.floor(Math.random() * slist.length)];
+  helper.setAttribute("placeholder", random);
+});
 
-    addTodo.reset()
-})
-
-todos.addEventListener('click', (e) => {
-
-    if (e.target.classList.contains('delete')) {
-        e.target.parentElement.remove()
-    }
-})
+todos.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
 
 const filterTodos = (term) => {
-    Array.from(todos.children)
-        .filter(todo => !todo.textContent.toLowerCase().includes(term))
-        .forEach(todo => todo.classList.add('filtered'))
+  Array.from(todos.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add("filtered"));
 
-    Array.from(todos.children)
-        .filter(todo => todo.textContent.toLowerCase().includes(term))
-        .forEach(todo => todo.classList.remove('filtered'))
-
-}
+  Array.from(todos.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
 
 //keyup event
-search.addEventListener('keyup', () => {
-    const term = search.value.trim().toLowerCase()
-    filterTodos(term)
-})
+search.addEventListener("keyup", () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+});
